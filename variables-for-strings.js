a23e7fa22a5b58135804a9121f9eb3a0d18173c7
Iqra Mansha <iqramansha78u@gmail.com>
2023-05-04 12:54:44 +0500

    First Commit

---
+++ variables-for-strings.js
@@ -0,0 +1,39 @@
+// #2 VARIABLES FOR STRINGS
+
+// Welcoming_my_users = "Thanks For Coming Here";
+// document.write(' Welcome ' + Welcoming_my_users);
+
+// Usind Console
+console.log("#2 VARIABLES FOR STRINGS");
+
+var username1;
+var myName = "Iqra Mansha";
+
+var message;
+var message = " Hello World ";
+alert(message);
+
+var name = " Iqra Mansha ";
+alert(name);
+
+var age = " 16 Years Old ";
+alert(age);
+
+var certificate = " Certified in Web Development ";
+alert(certificate);
+
+var x = " Pizza \n pizz \n piz \n pi \n p ";
+alert(x);
+
+var email = " My Email Address is iqramansha78u@gmail.com ";
+alert(email);
+
+var book = " “ A smarter way to learn JavaScript ” . ";
+var message1 = " I am trying to learn from a book A smarter way to learn JavaScript .... "
+alert(message1);
+
+var html = " YAH ! I can write HTML content through Javascript  ";
+document.write(html);
+
+var end = " “__________ஜ۩۞۩ஜ__________” "
+alert(end);
